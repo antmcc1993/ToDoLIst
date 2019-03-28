1f40583b6b3edb7385157110e9e6aa6a50191098
Anthony McClendon <anthonymcclendon@anthonys-macbook-air.local>
2019-03-28 04:48:45 -0500

    Layout for New ToDo

--- ToDoLIst/ToDoTableViewController.swift
+++ ToDoLIst/ToDoTableViewController.swift
@@ -14,19 +14,27 @@ class ToDoTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        if let savedToDos = ToDo.loadToDos() {
+            todos = savedToDos
+        } else {
+            ToDo.loadSampleToDos()
+        }
+        
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+    }
+    
+    
+    @IBAction func unwindToToDoList(segue: UIStoryboardSegue) {
+        
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -42,6 +50,18 @@ class ToDoTableViewController: UITableViewController {
         let todo = todos[indexPath.row]
         cell.textLabel?.text = todo.title
         return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            todos.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
     }
  
 
